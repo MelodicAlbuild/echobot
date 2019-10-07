8591a2ed6a86ae0106b5cfae05e37d3c7b3166b7
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 10:14:13 -0400

    Created Command for e!website

---
+++ EchoBot/commands/website.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let bicon = client.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+
+    .setDescription("Echo Studios Website")
+    .setAuthor("Echo Bot: By Vixel")
+    .setColor("#28a18f")
+    .addField("Offical Website", "https://www.echo-studios.com")
+    .setThumbnail(bicon)
+
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+  name: "youtube"
+}
