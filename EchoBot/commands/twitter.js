8c3eca478105bd0634420b5a841e96b5060001c9
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 10:43:09 -0400

    Made Twitter Embed for e!twitter

---
+++ EchoBot/commands/twitter.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let bicon = client.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+
+    .setDescription("Echo Studios Twitter")
+    .setAuthor("Echo Bot: By Vixel")
+    .setColor("#28a18f")
+    .addField("Twitter Handle", "https://twitter.com/echostudios1")
+    .setThumbnail(bicon)
+
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+  name: "twitter"
+}
