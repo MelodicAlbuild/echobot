8322252adf6726499883628dae96698175b5e288
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 11:06:45 -0400

    Added Embed for Discord Invite for e!discord

---
+++ EchoBot/commands/discord.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let bicon = client.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+
+    .setDescription("Echo Studios Discord")
+    .setAuthor("Echo Bot: By Vixel")
+    .setColor("#003182")
+    .addField("Discord Server", "https://www.discord.gg/qSU6xnN")
+    .setThumbnail(bicon)
+
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+  name: "discord"
+}
