c24e3641b0bc01bd54eecc4f35014e0de4cc00aa
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 10:55:39 -0400

    Added Patreon Embed for e!patreon

---
+++ EchoBot/commands/patreon.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let bicon = client.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+
+    .setDescription("Echo Studios Patreon")
+    .setAuthor("Echo Bot: By Vixel")
+    .setColor("#28a18f")
+    .addField("Patreon Page", "https://www.patreon.com/echostudios")
+    .setThumbnail(bicon)
+
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+  name: "patreon"
+}
