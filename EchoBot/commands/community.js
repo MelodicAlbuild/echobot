0d3da094e90e32733298201473ef60a40f1a65de
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 11:10:41 -0400

    Created Community Page that Has an Embed for All of the Links Requested

---
+++ EchoBot/commands/community.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    let bicon = client.user.displayAvatarURL;
+    let botEmbed = new Discord.RichEmbed()
+
+    .setDescription("Echo Studios Youtube")
+    .setAuthor("Echo Bot: By Vixel")
+    .setColor("#003182")
+    .addField("Youtube Channel", "https://www.youtube.com/channel/UCCk2MfTdmPu9FDm6PW0qh0g")
+    .addField("Discord Server", "https://www.discord.gg/qSU6xnN")
+    .addField("Offical Instagram", "https://www.instagram.com/echostudiosgames")
+    .addField("Patreon Page", "https://www.patreon.com/echostudios")
+    .addField("Twitter Handle", "https://twitter.com/echostudios1")
+    .setThumbnail(bicon)
+
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+  name: "community"
+}
