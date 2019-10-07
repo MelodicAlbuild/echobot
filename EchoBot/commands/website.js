38e4ef0bcbca41bfeb68ef143e4bea64a73da88f
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-10-07 13:09:25 -0400

    Updated File 21 of 22

--- EchoBot/commands/website.js
+++ EchoBot/commands/website.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     let botEmbed = new Discord.RichEmbed()
 
     .setDescription("Echo Studios Website")
-    .setAuthor("Echo Bot: By Vixel")
+    .setAuthor("Echo Bot: By Echo Studios")
     .setColor("#28a18f")
     .addField("Offical Website", "https://www.echo-studios.com")
     .setThumbnail(bicon)
